381ec00c9157268596d2a03ad047f0491cae2edf
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2021-07-14 21:00:54 +0300

    1

--- src/arrayLister/Main.java
+++ src/arrayLister/Main.java
@@ -1,9 +1,24 @@
 package arrayLister;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[]args) {
-		System.out.println("Hello ");
+		//1. Напишите программу на Java, чтобы создать новый список массивов,
+		//   добавить несколько цветов (строку) и распечатать коллекцию
+		ArrayList<String> colorList =new ArrayList<>();
+		
+		colorList.add("Green");
+		colorList.add("Yellow");
+		colorList.add("Blue");
+		
+		System.out.println(colorList);
+		//2. Напишите Java-программу для итерации всех элементов списка массивов
+		
+		for(String cl:colorList) {
+			System.out.print(cl+" ");
+		}
 	}
 
 }
